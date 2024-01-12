e874851c18b280dd65b939569960dd106df82653
Your Name <you@example.com>
2024-01-11 23:07:48 -0800

    Complete

--- src/Lecture02.java
+++ src/Lecture02.java
@@ -27,7 +27,7 @@ class Lecture02 {
 
         poem.forEach(word -> System.out.print(word + " "));
         System.out.println();
-        // The initial size of the list is 5 and the type is String.
+        // The initial capacity of the ArrayList is 5 and the type is String.
 
         // Put your answer for #4 here:
         // 4. Write code to insert two additional elements, "dark" and "and", at the
@@ -49,17 +49,39 @@ class Lecture02 {
         // Put your answer for #7 here:
         // 7. Write code to declare an ArrayList holding the first 10 multiples of 2: 0,
         // 2, 4,..., 18. Use a loop to fill the list with the proper elements.
+        ArrayList<Integer> multiplesOf2 = new ArrayList<>(10);
+        for (int i = 0; i < 10; i ++) {
+                multiplesOf2.add(i * 2);
+        }
+        for (int i = 0; i < multiplesOf2.size(); i++) {
+            System.out.print(multiplesOf2.get(i) + " ");
+        }
+        System.out.println();
 
         // Put your answer for #9 here:
         // 9. Write code to print out whether or not a list of Strings contains the
         // value "IS". Do not use a loop.
+        ArrayList<String> words = new ArrayList<>(Arrays.asList("Was", "IS", "Where", "Why", "When"));
 
+        if (words.contains("IS")) {
+            System.out.println("This list Contains \"IS\" ");
+        } else {
+            System.out.println("This list Does not contain \"IS\" ");
+        }
         // Put your answer for #14 here:
         // 14. What is a wrapper class? Describe the difference between an int and an
         // Integer.
 
+        // A wrapper class allows you to encapsulate primitive data types, such as char, int, double, and boolean,
+        // as objects.
+        // int is a primitive data type that stores a number but cannot be used in an object setting.
+        // Integer is the wrapper class for int that allows its use in ArrayList and other object use cases.
+
         // Put your answer for #13 here:
         // 13. The code that follows does not compile. Why not? Explain how to fix it.
+
+        // The code that follows does not compile because the ArrayList is initialized utilizing a primitive data type.
+        // For this code to work, we need to utilize the wrapper class, 'Integer', to encapsulate the primitive data type 'int'.
 
         // ArrayList<int> numbers = new ArrayList<>();
         // numbers.add(7);
@@ -73,5 +95,24 @@ class Lecture02 {
         // A. [2, 6, 1, 8]
         // B. [30, 20, 10, 60, 50, 40]
         // C. [-4, 16, 9, 1, 64, 25, 36, 4, 49]
+        ArrayList<Integer> list = new ArrayList<>(Arrays.asList(-4, 16, 9, 1, 64, 25, 36, 4, 49));
+        mystery1(list);
+        for (Integer num : list) {
+            System.out.print(num + " ");
+        }
+        // A. 1 2 6 8
+        // B. 10 30 40 20 60 50
+        // C. -4 1 25 4 16 9 64 36 49
     }
+    public static void mystery1(ArrayList<Integer> list) {
+
+        for (int i = list.size() - 1; i > 0; i--) {
+            if (list.get(i) < list.get(i - 1)) {
+                int element = list.get(i);
+                list.remove(i);
+                list.add(0, element);
+            }
+        }
+    }
+
 }
